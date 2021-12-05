f9ede26c3ce91458f5cb81101cc5695a6c7a9ad4
vladimir <volt901@icloud.com>
2021-12-05 09:29:26 +0500

    Initial Commit

---
+++ second/secondApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondApp.swift
+//  second
+//
+//  Created by Владимир Ширяев on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct secondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
